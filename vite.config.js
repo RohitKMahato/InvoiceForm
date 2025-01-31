c46b9e8b16ad1e998cfd90676f112cc51e3406d4
RohitKMahato <rohitmahato1235@gmail.com>
2025-01-31 14:48:36 +0530

    Fixed deployment issue

--- vite.config.js
+++ vite.config.js
@@ -38,6 +38,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/InvoiceForm/',
   resolve: {
     alias: {
       '/components': '/src/components',
