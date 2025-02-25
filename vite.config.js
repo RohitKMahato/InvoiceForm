35ca7a73a28ba72ef8831487909575b69f511581
RohitKMahato <rohitmahato1235@gmail.com>
2025-02-25 10:59:38 +0530

    Updated code with latest changes

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,54 @@
 
 
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server:{
+//     host: '0.0.0.0',
+//     hmr: {
+//       overlay: false
+//     },
+//     fs:{
+//       strict: false,
+//     },
+//   },
+//   base: '/InvoiceForm/',
+//   resolve: {
+//     alias: {
+//       '/components': '/src/components',
+//     },
+//   },
+
+//   css: {
+//     postcss: `./postcss.config.js`,
+//   }
+// });
+
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server:{
+  server: {
     host: '0.0.0.0',
     hmr: {
       overlay: false
     },
-    fs:{
+    fs: {
       strict: false,
     },
   },
-  base: '/InvoiceForm/',
+  base: '/InvoiceForm/', // Change this if your GitHub repo name is different
   resolve: {
     alias: {
       '/components': '/src/components',
     },
   },
-
   css: {
-    postcss: `./postcss.config.js`,
+    postcss: './postcss.config.js',
   }
 });
-
